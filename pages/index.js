82b3eec76dfad355d59dc90769c0139c5f8380e1
imanol Rodríguez de maya <imanolrodriguezdemaya@imanols-MBP.home>
2021-03-07 11:22:06 +0100

    fixes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import { SelectMode } from '../components/SelectMode';
-import { getMonth } from '../helpers/nextGP';
 
 export default function Home() {
-  getMonth()
-
   return (
     <div>
       <SelectMode />
